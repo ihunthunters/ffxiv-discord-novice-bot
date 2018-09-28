5ac40161d41e03712880af1b102fc66fe9355556
Hunter James <hunterjames.fb@gmail.com>
2018-09-28 13:11:05 -0400

    Added the Jobs Command
    Added the jobs command to get info about the specified job/class.

--- NoviceGuide.py
+++ NoviceGuide.py
@@ -37,8 +37,24 @@ async def mechanics(ctx, *args):
 
     await ctx.send(bot_respone)
 
+@bot.command()
+async def job(ctx, *args):
+    target_job = ""
+
+    for val in args:
+        target_job += (val + ' ')
+
+    overview_link = getJobDetails(str.strip(target_job.lower()))
+
+    if overview_link != 'nothing':
+        bot_respone = ("Here is a link to help with %s: \n%s" % (target_job, overview_link))
+    else:
+        bot_respone = ("Sorry, I could not find anything helpful for %s.  Maybe you meant to lookup something else?" % target_job)
+    
+    await ctx.send(bot_respone)
+
 def getHelpDescription():
-    return '```General \n\n info \n\treturns info about me. \n\n mechanics {{dungeon_name}} \n\t returns information about the requested dungeon/raid/duty/boss```'
+    return '```General \n\n info \n\treturns info about me. \n\n mechanics {{dungeon_name}} \n\t returns information about the requested dungeon/raid \n\n jobs {{job_name}} \n\t returns information about the requested job/class```'
 
 def getMechanicVideo(arg):
     video_links = {
@@ -52,5 +68,40 @@ def getMechanicVideo(arg):
         "sunken temple of qarn": "https://www.youtube.com/watch?v=q4GS1N9pMcI"
     }
     return video_links.get(arg, "nothing")
+
+def getJobDetails(arg):
+    wiki_links = {
+        "lancer": "lancer",
+        "rogue": "rogue",
+        "archer": "archer",
+        "gladiator": "gladiator",
+        "pugilist": "pugilist",
+        "marauder": "marauder",
+        "conjurer": "conjurer",
+        "thaumaturge": "thaumaturge",
+        "arcanist": "archanist",
+        "warrior": "warrior",
+        "paladin": "paladin",
+        "monk": "monk",
+        "dragoon": "dragoon",
+        "bard": "bard",
+        "white mage": "white_mage",
+        "black mage": "black_mage",
+        "summoner": "summoner",
+        "scholar": "scholar",
+        "ninja": "ninja",
+        "dark knight": "dark_knight",
+        "astrologian": "astrologian",
+        "machinist": "machinist",
+        "samurai": "samurai",
+        "red mage": "red_mage"
+    }
+
+    if arg in wiki_links:
+        wiki_link = "https://ffxiv.consolegameswiki.com/wiki/%s" % wiki_links.get(arg)
+    else:
+        wiki_link = "nothing"
+
+    return wiki_link
 
 bot.run("token")
