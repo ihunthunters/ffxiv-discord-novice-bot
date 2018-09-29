d1bf035efc42987c3166be07c456d7b264730c0d
Hunter James <hunterjames.fb@gmail.com>
2018-09-28 21:45:44 -0400

    Added Hunting Logs
    Added hunting logs command and added general application logging.

--- NoviceGuide.py
+++ NoviceGuide.py
@@ -3,7 +3,15 @@ import discord
 import traceback
 import json
 import os
+import logging
 from discord.ext import commands
+
+#logging setup
+logger = logging.getLogger('discord')
+logger.setLevel(logging.ERROR)
+handler = logging.FileHandler(filename='novice_bot.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 #basic bot setup
 bot = commands.Bot(command_prefix='*')
@@ -15,6 +23,13 @@ def getAuthToken():
 
     return data["authToken"]
 
+#setup the error handling
+@bot.event
+async def on_command_error(ctx, error):
+    embed = discord.Embed(title='An Error Has Occured', description='Sorry, something wen''t wrong.  I will log the error and have Hunter fix it later.', colour=0xFF0000)
+
+    await ctx.send(embed=embed)    
+
 #setup the command listeners
 @bot.command()
 async def info(ctx):
@@ -24,7 +39,10 @@ async def info(ctx):
 
 @bot.command()
 async def help(ctx):
-    await ctx.send(getHelpDescription())
+    description = getHelpDescription()
+    embed = discord.Embed(title='Commands Help', description=description, author='Hunter James', colour=0x00FF00)
+    
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def mechanics(ctx, *args):
@@ -43,7 +61,7 @@ async def mechanics(ctx, *args):
     await ctx.send(bot_respone)
 
 @bot.command()
-async def job(ctx, *args):
+async def jobs(ctx, *args):
     target_job = ""
 
     for val in args:
@@ -52,14 +70,28 @@ async def job(ctx, *args):
     overview_link = getJobDetails(str.strip(target_job.lower()))
 
     if overview_link != 'nothing':
-        bot_respone = ("Here is a link to help with %s: \n%s" % (target_job, overview_link))
+        bot_response = ("Here is a link to help with %s: \n%s" % (target_job, overview_link))
     else:
-        bot_respone = ("Sorry, I could not find anything helpful for %s.  Maybe you meant to lookup something else?" % target_job)
+        bot_response = ("Sorry, I could not find anything helpful for %s.  Maybe you meant to lookup something else?" % target_job)
     
-    await ctx.send(bot_respone)
+    await ctx.send(bot_response)
+
+@bot.command()
+async def hunting_logs(ctx, *args):
+    args_list = [arg for arg in args]
+    target_job = ''.join(args_list)
+
+    log_link = getHuntingLogs(str.strip(target_job.lower()))
+
+    if log_link != 'nothing':
+        bot_response = ("Here is a link to help with %s: \n%s" % (target_job, log_link))
+    else:
+        bot_response = ("Sorry, I could not find anything helpful for %s.  Maybe you meant to lookup something else?" % target_job)
+ 
+    await ctx.send(bot_response)        
 
 def getHelpDescription():
-    return '```General \n\n info \n\treturns info about me. \n\n mechanics {{dungeon_name}} \n\t returns information about the requested dungeon/raid \n\n jobs {{job_name}} \n\t returns information about the requested job/class```'
+    return '---------- General ----------\n\n info \n\treturns info about me. \n\n mechanics {{dungeon_name}} \n\t returns information about the requested dungeon/raid \n\n jobs {{job_name}} \n\t returns information about the requested job/class \n\n hunting_logs {{job_name}} \n\treturns information about that classes hunting logs \n\n ---------- In Development ----------\n\n professions {{job_name}} \n\treturns information about a specific profession'
 
 def getMechanicVideo(arg):
     video_links = {
@@ -108,5 +140,26 @@ def getJobDetails(arg):
         wiki_link = "nothing"
 
     return wiki_link
+
+def getHuntingLogs(arg):
+    log_links = {
+        "lancer": "lancer",
+        "rogue": "rogue",
+        "archer": "archer",
+        "gladiator": "gladiator",
+        "pugilist": "pugilist",
+        "marauder": "marauder",
+        "conjurer": "conjurer",
+        "thaumaturge": "thaumaturge",
+        "arcanist": "archanist",
+    }
+
+    if arg in log_links:
+        log_link = "https://ffxiv.consolegameswiki.com/wiki/%s_Hunting_Log" % log_links.get(arg)
+    else:
+        log_link = "nothing"
+
+    return log_link
+
 
 bot.run(getAuthToken())
